f414f17a0093fecb88cedb37c37b3dda36bc3091
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2025-12-14 21:29:17 +0530

    "Location_Corrected_of_Files"

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './index.css';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
