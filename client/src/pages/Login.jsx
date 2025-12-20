839d62ebe48970d216b3d53a0ad723c0a5ef6ca1
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2025-12-20 21:16:16 +0530

    "Login_and_SignUp_Updated"

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,34 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Logging in with", { email, password });
+  };
   return (
-    <form className="form" id="login-page">
+    <form  onSubmit={handleSubmit} className="form" id="login-page">
       <h2>Login</h2>
       <label htmlFor="email">Email Address</label>
-      <input type="text" id="email" placeholder="Enter Your Email"/>
+      <input 
+        type="email" 
+        id="email" 
+        placeholder="Enter Your Email"
+        value={email}
+        required
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <label htmlFor="password">Password</label>
-      <input type="password" id="password" placeholder="Enter Your Password"/>
-      <button className="loginButton">Login</button>
+      <input 
+        type="password" 
+        id="password" 
+        placeholder="Enter Your Password"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit" className="loginButton">Login</button>
       <p>Don't have an account? <Link to="/signup">SignUp</Link></p>
     </form>
   )
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,45 @@
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("Account created and logged in successfully");
+}
+
   return (
-    <form className="form" id="signup-page">
+    <form onSubmit={handleSubmit} className="form" id="signup-page">
       <h2>SignUp</h2>
       <label htmlFor="name">Enter Your Name</label>
-      <input type="text" id="name" placeholder="Enter Your Name"/>
+      <input 
+        type="text" 
+        id="name" 
+        placeholder="Enter Your Name"
+        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label htmlFor="email">Enter Your Email</label>
-      <input type="text" id="email" placeholder="Enter Your Email"/>
+      <input 
+        type="email" 
+        id="email" 
+        placeholder="Enter Your Email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <label htmlFor="password">Enter Your Password</label>
-      <input type="password" id="password" placeholder="Enter Your Password"/>
+      <input 
+        type="password" 
+        id="password" 
+        placeholder="Enter Your Password"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button className="signup-button">SignUp</button>
       <p>Already have an account? <Link to="/login">Login</Link></p>
     </form>
