d3ca69ddd7a1e3fc2e14fac4dae751de5a15b0f8
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2025-12-20 16:03:39 +0530

    "Css_Updated"

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>LearnFlow</h1>
-
-      <div className="links">
-        <Link to="/login">Pricing</Link>
-        <Link to="/login">Contact</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup" className="signup-link">Sign Up</Link>
+      <div className="elements">LearnFlow</div>
+      <div className="elements">
+        <div className="links">
+          <Link to="/login">Pricing</Link>
+          <Link to="/login">Contact</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">SignUp</Link>
+        </div>
       </div>
     </nav>
   );
