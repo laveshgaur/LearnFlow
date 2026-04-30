f66ecf680ccf26c2608392845e78560846e55e41
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2026-04-30 13:28:30 +0530

    "UserService_Updated"

--- Backend/src/main/java/com/lms/service/UserService.java
+++ Backend/src/main/java/com/lms/service/UserService.java
@@ -20,7 +20,7 @@ public class UserService {
     public User createUser(User user) {
         user.setCreatedAt(LocalDateTime.now());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER", "INSTRUCTOR", "ADMIN"));
+        user.setRoles(Arrays.asList("USER"));
         return userRepository.save(user);
     }
 
